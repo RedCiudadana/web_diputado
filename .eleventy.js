700713e402dbd989f9e00a0b2fa358bdbf10df6e
MarianoReyes <josemarianoreyesh02@gmail.com>
2023-10-26 10:26:32 -0600

    trabajos en inicio

---
+++ .eleventy.js
@@ -0,0 +1,31 @@
+const rmj = require('render-markdown-js')
+
+module.exports = function (eleventyConfig) {
+
+    eleventyConfig.setTemplateFormats("njk,html,md");
+    
+    eleventyConfig.addPassthroughCopy('src');
+    eleventyConfig.addPassthroughCopy('admin');
+    eleventyConfig.addPassthroughCopy('assets');
+
+    eleventyConfig.addNunjucksFilter("rmj", function(content) {
+        return rmj(content);
+    });
+
+    eleventyConfig.addNunjucksFilter("limit", function (array, limit) {
+        return array.slice(0, limit);
+    });
+
+    eleventyConfig.addNunjucksFilter("limitPart", function(array, limit1, limit2) {
+        return array.slice(limit1, limit2);
+    });
+
+    eleventyConfig.addFilter("dateFormat", function(date, format) {
+        return moment(date).format(format);
+    });
+
+    eleventyConfig.addFilter("log", function(variable) {
+        console.log(variable);
+        return variable;
+    });
+}
