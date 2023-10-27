dd139e41e1cf9f618d55bfcf8b70425f2ffca512
José Mariano Reyes <josemarianoreyesh02@gmail.com>
2023-10-27 09:46:46 -0600

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 const rmj = require('render-markdown-js');
 const moment = require('moment');
-
+ 
 module.exports = function (eleventyConfig) {
 
     eleventyConfig.setTemplateFormats("njk,html,md");
